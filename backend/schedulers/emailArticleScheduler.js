5c127df533ac26f1f25011cb5a2caeca45564317
Aman <20031010.aman@gmail.com>
2025-03-19 01:54:43 +0530

    Testing scheduler

--- backend/schedulers/emailArticleScheduler.js
+++ backend/schedulers/emailArticleScheduler.js
@@ -5,21 +5,25 @@ const { sendArticle } = require("../utils/nodemailerUtil.js");
 
 // Define your task that should run at 8 AM IST
 const task = async () => {
-    
-    console.log("inside tasks")
-    const articles = await Article.find({});
-    const users = await User.find({});
+    try {
+        console.log("inside tasks");
+        const articles = await Article.find({});
+        const users = await User.find({});
 
-    const todaysArticle = articles[Math.floor(Math.random() * articles.length)];
+        const todaysArticle = articles[Math.floor(Math.random() * articles.length)];
 
-    for (let i = 0; i < users.length; i++) {
-        sendArticle(users[i].email, todaysArticle.title, todaysArticle.excerpt);
+        for (let i = 0; i < users.length; i++) {
+            await sendArticle(users[i].email, todaysArticle.title, todaysArticle.excerpt);
+        }
+    } catch (error) {
+        console.error("Error executing cron job:", error);
     }
 };
 
+
 // Schedule the cron job to run at 8 AM IST every day
-cron.schedule("0 8 * * *", task, {
+cron.schedule("56 1 * * *", task, {
     timezone: "Asia/Kolkata", // Set the timezone to IST
 });
 
-console.log("Cron job scheduled to run at 8 AM IST every day");
+console.log("Cron job scheduled to run at 1:56 IST every day");
